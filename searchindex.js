c5c8c5a12e8a99436f4e5fb4007b362ad4892169
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2016-08-08 14:12:38 -0300

    Update content for GH pages.

---
+++ searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:49,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Personal Development Initiative&#8217;s documentation!"],titleterms:{develop:0,document:0,indice:0,initi:0,person:0,tabl:0,welcom:0}})
